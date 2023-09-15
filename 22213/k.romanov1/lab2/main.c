c864b23b725fed25ef5bcfb7a73106fdcafc7bd8
Kirill Romanov <k.romanov1@g.nsu.ru>
2023-09-15 12:04:06 +0700

    changed main.c

---
+++ 22213/k.romanov1/lab2/main.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+ 
+int main()
+{
+        time_t now = time(NULL);
+        const int lenBuf = 26;
+        char * buf = (char*)malloc(sizeof(char)*lenBuf);
+        putenv("TZ=GMT+7");
+        printf("%s\n", ctime_r(&now, buf, lenBuf));
+        return 0;
+}
+
+
