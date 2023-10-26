f8765b832013c97c5cbb5945f0711550eff2420d
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-10-26 11:48:34 +0700

    Create lab_9.c

---
+++ 22213/a.shushakov1/lab_9.c
@@ -0,0 +1,27 @@
+#include <wait.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(){
+    printf("\nDoing shit\n\n");
+    pid_t pid_1 = fork();
+    switch(pid_1){
+        case(-1):
+            exit(EXIT_FAILURE);
+            break;
+        case(0):
+            int ifError = execl("/bin/cat","cat","1.txt",(char *)0);
+            if(ifError < 0)
+                exit(EXIT_FAILURE);
+            break;
+        default:
+            pid_t ifError0 = wait(0);
+            if(ifError0 < 0)
+                exit(EXIT_FAILURE);
+            printf("\n\nDone\n");
+            break;
+    }
+    exit(EXIT_SUCCESS);
+}
