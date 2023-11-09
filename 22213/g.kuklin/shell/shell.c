917a6bd783ff1008d1721d038f925368231379de
Kuklin Georgiy <egorka99966@gmail.com>
2023-11-09 11:11:27 +0700

    Superflous if. Check for completed jobs even on invalid command. Wrap around for job indices

--- 22213/g.kuklin/shell/shell.c
+++ 22213/g.kuklin/shell/shell.c
@@ -34,7 +34,7 @@ void execute_command(int id) {
     if (cmds[id].cmdflag & INFILE) {
         int in = open(cmds[id].infile, O_RDONLY);
         if (in == -1) {
-            perror("Failed to open file");    
+            perror("Failed to open file"); 
             return;
         }
         if (dup2(in, 0) == -1) {
@@ -42,13 +42,9 @@ void execute_command(int id) {
             return;
         }
     }
-    if (execvp(cmds[id].cmdargs[0], cmds[id].cmdargs) == -1) {
-        char message[1024] = "Failed to execute command ";
-        perror(strcat(message, cmds[id].cmdargs[0]));
-        return;
-    }
-    assert(0);
-    return;
+    execvp(cmds[id].cmdargs[0], cmds[id].cmdargs);
+    char message[1024] = "Failed to execute command ";
+    perror(strcat(message, cmds[id].cmdargs[0]));
 }
 
 int main(int argc, char *argv[]) {
@@ -57,6 +53,7 @@ int main(int argc, char *argv[]) {
     int ncmds;
     char prompt[50];      /* shell prompt */
 
+    // TODO(theblek): make this a linked list of jobs by encoding next free as a negative number
     pid_t jobs[JOBS_BUFFER_SIZE] = {0};
     int next_job = 0;
 
@@ -65,6 +62,22 @@ int main(int argc, char *argv[]) {
     sprintf(prompt,"[%s] ", argv[0]);
 
     while (promptline(prompt, line, sizeof(line)) > 0) {    /* until eof  */
+        // Check for completed jobs
+        for (int i = 0; i < JOBS_BUFFER_SIZE; i++) {
+            if (!jobs[i]) continue;
+
+            siginfo_t info;
+            if (waitid(P_PID, jobs[i], &info, WEXITED | WNOHANG) == -1) {
+                perror("Failed to wait for a job");
+                continue;
+            }
+            if (info.si_pid != 0) {
+                printf("[%d] %d Finished\n", i, info.si_pid);
+                jobs[i] = 0;
+                next_job = i;
+            }
+        }
+
         if ((ncmds = parseline(line)) <= 0) {
             #ifdef DEBUG
             fprintf(stderr, "Unrecognised command\n");
@@ -86,39 +99,32 @@ int main(int argc, char *argv[]) {
             pid_t child;
             siginfo_t child_info;
             switch (child = fork()) {
-                case 0:
-                    execute_command(i);
-                    break;
                 case -1:
                     perror("Failed to fork");
                     return 1; 
+                case 0:
+                    execute_command(i);
+                    break;
                 default:
                     if (bkgrnd) {
+                        if (next_job == -1) {
+                            printf("Out of job slots");
+                        }
                         printf("[%d] %d\n", next_job, child);
                         jobs[next_job] = child;
-                        while (jobs[++next_job] != 0) {}
-                    } else {
-                        if (waitid(P_PID, child, &child_info, WEXITED) == -1) {
-                            perror("Failed to wait for child");    
+                        // Find next free job handle
+                        int initial = next_job;
+                        while (jobs[next_job++] != 0 && next_job != initial) {
+                            if (next_job == JOBS_BUFFER_SIZE) {
+                                next_job = 0;
+                            }
                         }
+                        next_job = -1; // There is no empty slot
+                    } else if (waitid(P_PID, child, &child_info, WEXITED) == -1) {
+                        perror("Failed to wait for child");
                     }
             }
         }
-
-        for (int i = 0; i < JOBS_BUFFER_SIZE; i++) {
-            if (!jobs[i]) continue;
-
-            siginfo_t info;
-            if (waitid(P_PID, jobs[i], &info, WEXITED | WNOHANG) == -1) {
-                perror("Failed to wait for a job"); 
-                continue;
-            }
-            if (info.si_pid != 0) {
-                printf("[%d] %d Finished\n", i, info.si_pid);
-                jobs[i] = 0;
-            }
-        }
-
     }/* close while */
 }
 /* PLACE SIGNAL CODE HERE */
