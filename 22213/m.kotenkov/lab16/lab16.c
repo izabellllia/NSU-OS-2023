233f3b614ba538da8631cd213c20e19cfa5fbccb
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-11-23 21:16:15 +0700

    modified

--- 22213/m.kotenkov/lab16/lab16.c
+++ 22213/m.kotenkov/lab16/lab16.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 #include <termios.h>
 
 
 int main() {
-    struct termios tdes;
-    tcgetattr(0, &tdes);
-
-    tdes.c_lflag &= ~ICANON;
-    tdes.c_cc[VMIN] = 1;
-
-    tcsetattr(0, TCSAFLUSH, &tdes);
-
-
     char inp;
-    printf("Please say yes\n");
-    scanf("%c", &inp);
+    struct termios prev_term, new_term;
 
-    if (inp == 'y' || inp == 'Y')
-    {
-        printf("\nOh yeah...\n");
-    } else
-    {
-        printf("\nOh no(((\n");
+    tcgetattr(0, &prev_term);
+
+    new_term = prev_term;
+    new_term.c_lflag &= ~ICANON;
+    new_term.c_cc[VMIN] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_term) == -1) {
+        perror("Tcsetattr failure");
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
+    if ((write(1, "Please say yes\n", 16)) == -1) {
+        perror("Write failure");
+        exit(EXIT_FAILURE);
+    }
+    if ((read(0, &inp, 1)) == -1) {
+        perror("Read failure");
+        exit(EXIT_FAILURE);
+    }
+
+    if (inp == 'y' || inp == 'Y') {
+        if ((write(1, "\nOh yeah...\n", 13)) == -1) {
+            perror("Write failure");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if ((write(STDIN_FILENO, "\nOh no(((\n", 11)) == -1) {
+            perror("Write failure");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &prev_term) != 0) {
+        perror("Tcsetattr failure");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
 }
