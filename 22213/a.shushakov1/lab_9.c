4e076aa10e4581f46138732a3f0f0ebfe2a04edf
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-12-07 12:04:17 +0700

    Update lab_9.c

--- 22213/a.shushakov1/lab_9.c
+++ 22213/a.shushakov1/lab_9.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+    
 int main(){
+    int ifError;
+    int child_status;
+    pid_t ifError0;
     printf("\nDoing shit\n\n");
     pid_t pid_1 = fork();
+ 
     switch(pid_1){
         case(-1):
             exit(EXIT_FAILURE);
             break;
         case(0):
-            int ifError = execlp("cat","cat","1.txt",(char *)0);
+            ifError = execlp("cat","cat","1.txt",(char *)0);
             if(ifError < 0)
                 exit(EXIT_FAILURE);
             break;
         default:
-            pid_t ifError0 = wait(0);
+            ifError0 =  ifError0 = waitpid(pid_1, &child_status, WUNTRACED);
             if(ifError0 < 0)
                 exit(EXIT_FAILURE);
             printf("\n\nDone\n");
