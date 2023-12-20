e3a2288e8b99231cc7195b2dc4f3c0f6ae4e7c54
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-21 04:09:32 +0700

    lab27

---
+++ 22213/d.dolgov/lab27/lab27.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 256
+
+int main(int argc, char **argv) {
+    FILE *fin, *fout;
+    char line[BUFFER_SIZE];
+
+    if ((fin = fopen(argv[1], "r")) == NULL) {
+        perror("Fopen failed");
+    }
+
+    fout = popen("wc -l", "w");
+
+    if (fout == NULL) {
+        fclose(fin);
+        perror("Cannot allocate memory");
+    }
+
+    while (fgets(line, BUFFER_SIZE, fin) != NULL) {
+        if (line[0] == '\n') {
+            fputs(line, fout);
+        }
+    }
+
+    fclose(fin);
+    pclose(fout);
+    exit(0);
+}
