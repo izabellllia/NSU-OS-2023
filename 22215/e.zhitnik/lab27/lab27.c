15ddd277c652f6f4524a0014397679e66ec63333
Elizaveta Zhitnik <e.zhitnik@g.nsu.ru>
2024-09-06 20:34:09 +0700

    lab27

---
+++ 22215/e.zhitnik/lab27/lab27.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    char input[BUFSIZ];
+    FILE *fin, *fpout;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    if ((fin = fopen(argv[1], "r")) == NULL) {
+        perror(argv[1]);
+        return 1;
+    }
+
+    if ((fpout = popen("wc -l", "w")) == NULL) {
+        perror("popen");
+        fclose(fin);
+        return 1;
+    }
+
+    while (fgets(input, BUFSIZ, fin) != NULL) {
+        if (strlen(input) == 1 && input[0] == '\n') {
+            fputs(input, fpout);
+            fflush(fpout);
+        }
+    }
+
+    fclose(fin);
+    pclose(fpout);
+    return 0;
+}
+
